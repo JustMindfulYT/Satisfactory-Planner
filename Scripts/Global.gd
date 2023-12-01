2bbdd66a4d5c97f12e81ca7ec537b287f449e021
JustMindfulYT <master51.main@gmail.com>
2023-12-01 14:32:21 +0100

    Valid Project check, ...

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -31,7 +31,6 @@ const default_settings : Dictionary = {
 func _ready():
 	print("'Global' initialized")
 	var a = loadJson("user://settings.json")
-	print(a)
 	self.current_settings = a[0]
 	check_settings(a[1])
 
@@ -93,6 +92,156 @@ func check_settings(exists : bool):
 	printerr("'check_settings' hasnt been fully implemented yet")
 	
 	print("check_settings - end")
+
+
+func check_project_valid(project : Dictionary):
+	if not(project.has("info") and project["info"] is Dictionary):
+		return [false, "'info' key missing or not a Dictionary"]
+	else:
+		if not(project.info.has("name") and project.info["name"] is String):
+			return [false, "'info/name' key missing or not a String"]
+		if not(project.info.has("creator") and project.info["creator"] is String):
+			return [false, "'info/creator' key is missing or not a String"]
+	if not(project.has("production_lines") and project["production_lines"] is Dictionary):
+		return [false, "'production_lines' key is missing or not a Dictionary"]
+	else:
+		for line in project.production_lines.keys():
+			var a = check_production_line(line, project.production_lines[line])
+			if a[0]:
+				continue
+			else:
+				return [false, a[1]]
+		return [true, ""]
+
+func check_production_line(name : String, line : Dictionary):
+	### Name
+	if not(line.has("name") and line["name"] is String):
+		return [false, "'production_lines/%s/name' is missing or not a Dictionary" % name]
+	### Resources
+	if not(line.has("resources") and line["resources"] is Dictionary):
+		return [false, "'production_lines/%s/resources' is missing or not a Dictionary" % name]
+	else:
+		for resource in line.resources:
+			if not(resource is int):
+				return [false, "'production_lines/%s/resources/%s' is not a Number" % [name, resource]]
+	### Input
+	if not(line.has("input") and line["input"] is Dictionary):
+		return [false, "'production_lines/%s/input' is missing or not a Dictionary" % name]
+	else:
+		for input in line.input:
+			if not(input is int):
+				return [false, "'production_lines/%s/input/%s' is not a Number" % [name, input]]
+	### Output
+	if not(line.has("output") and line["output"] is Dictionary):
+		return [false, "'production_lines/%s/output' is missing or not a Dictionary" % name]
+	else:
+		for output in line.output:
+			if not(output is int):
+				return [false, "'production_lines/%s/output/%s' is not a Number" % [name, output]]
+	### Power
+	if not(line.has("power") and line["power"] is Dictionary):
+		return [false, "'production_lines/%s/power' is missing or not a Dictionary" % name]
+	else:
+		## Total
+		if not(line.power.has("total") and line.power["total"] is int):
+			return [false, "'production_lines/%s/power/total' is missing or not a Number" % name]
+		## Buildings
+		if not(line.power.has("buildings") and line.power["buildings"] is Dictionary):
+			return [false, "'production_lines/%s/power/buildings' is missing or not a Dictionary" % name]
+		else:
+			for building in line.power.buildings.keys():
+				var a = check_building_power(name, building, line.power.buildings[building])
+				if a[0]:
+					continue
+				else:
+					return [false, a[1]]
+	### Items
+	if not(line.has("items") and line["items"] is Dictionary):
+		return [false, "'production_lines/%s/items' is missing or not a Dictionary" % name]
+	else:
+		for item in line.items.keys():
+			var a = check_item(name, item, line.items[item])
+			if a[0]:
+				continue
+			else:
+				return [false, a[1]]
+	return [true, ""]
+
+func check_building_cost(name : String, b_name : String, b : Dictionary):
+	# Times
+	if not(b.has("times") and b["times"] is int):
+		return [false, "'production_lines/%s/power/buildings/%s/times' is missing or not a Number" % [name, b_name]]
+	# Cost
+	if not(b.has("cost") and b["cost"] is Dictionary):
+		return [false, "'production_lines/%s/power/buildings/%s/cost' is missing or not a Dictionary" % [name, b_name]]
+	else:
+		for c in b.cost:
+			if not(c is int):
+				return [false, "'production_lines/%s/buildings/%s/cost/%s' is not a Number" % [name,b_name ,c]]
+	return [true, ""]
+
+func check_building_power(name : String, b_name : String, b : Dictionary):
+	# Times
+	if not(b.has("times") and b["times"] is int):
+		return [false, "'production_lines/%s/power/buildings/%s/times' is missing or not a Number" % [name, b_name]]
+	# Consumption
+	if not(b.has("consumption") and b["consumption"] is int):
+		return [false, "'production_lines/%s/power/buildings/%s/consumption' is missing or not a Number" % [name, b_name]]
+	# Recipes
+	if not(b.has("recipes") and b["recipes"] is Dictionary):
+		return [false, "'production_lines/%s/power/buildings/%s/recipes' is missing or not a Dictionary" % [name, b_name]]
+	else:
+		for recipe in b.recipes.keys():
+			var a = check_recipe(name, b_name, recipe, b.recipes[recipe])
+			if a[0]:
+				continue
+			else:
+				return [false, a[1]]
+	return [true, ""]
+
+func check_recipe(name : String, b_name : String, r_name : String, r : Dictionary):
+	# Times
+	if not(r.has("times") and r["times"] is int):
+		return [false, "'production_lines/%s/power/buildings/%s/recipes/%s/times' is missing or not a Number" % [name, b_name, r_name]]
+	# Consumption
+	if not(r.has("consumption") and r["consumption"] is int):
+		return [false, "'production_lines/%s/power/buildings/%s/recipes/%s/consumption' is missing or not a Number" % [name, b_name, r_name]]
+	return [true, ""]
+
+func check_item(name : String, i_name : String, i : Dictionary):
+	# production
+	if not(i.has("production") and i["production"] is Dictionary):
+		return [false, "'production_lines/%s/items/%s/production' is missing or not a Dictionary" % [name, i_name]]
+	else:
+		for item in i.production.keys():
+			var a = check_item_a(name, i_name, "production", i.production[item])
+			if a[0]:
+				continue
+			else:
+				return [false, a[1]]
+	if not(i.has("consumption") and i["consumption"] is Dictionary):
+		return [false, "'production_lines/%s/items/%s/consumption' is missing or not a Dictionary" % [name, i_name]]
+	else:
+		for item in i.production.keys():
+			var a = check_item_a(name, i_name, "consumption", i.production[item])
+			if a[0]:
+				continue
+			else:
+				return [false, a[1]]
+	return [true, ""]
+
+func check_item_a(name : String, i_name : String, p : String, i : Dictionary):
+	# ips
+	if not(i.has("ips") and i["ips"] is int):
+		return [false, "'production_lines/%s/items/%s/%s/ips' is missing or not a Number" % [name, i_name, p]]
+	# items
+	if not(i.has("items") and i["items"] is Dictionary):
+		return [false, "'production_lines/%s/items/%s/%s/items' is missing or not a Dictionary" % [name, i_name, p]]
+	else:
+		for item in i.keys():
+			if not(item is int):
+				return [false, "'production_lines/%s/items/%s/%s/items/%s' is not a Number" % [name, i_name, p, item]]
+	return [true, ""]
 
 func saveJson(path : String, json : Dictionary):
 	var f = File.new()
--- Scripts/Play.gd
+++ Scripts/Play.gd
@@ -1,14 +1,57 @@
 extends Control
 
+var projects : Array = []
+
+var selected_item : int = -1
+
+
+func _ready():
+	load_projects()
+	for project in projects:
+		pass
+
+func load_projects():
+	var d = Directory.new()
+	if not(d.dir_exists("user://projects")):
+		d.make_dir("user://projects")
+		return
+	d.open("user://projects")
+	var f_names : Array = []
+	d.list_dir_begin()
+	var f_name = d.get_next()
+	f_names.append(f_name)
+	while f_name != "":
+		f_name = d.get_next()
+		f_names.append(f_name)
+	
+	for file in f_names:
+		var p : Dictionary = Global.loadJson("user://projects/%s" % file)
+		var a : Array = Global.check_project_valid(p)
+		if a[0] == false:
+			return
+		self.projects.append(p)
+
 
 
 func _on_AddButton_button_up():
-	pass # Replace with function body.
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Scenes/NewProject.tscn")
 
 
 func _on_OpenButton_button_up():
-	pass # Replace with function body.
+	pass
 
 
 func _on_CancelButton_button_up():
-	pass # Replace with function body.
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Scenes/Main.tscn")
+
+
+func _on_ProjectList_item_selected(index):
+	selected_item = index
+	$OpenButton.disabled = false
+
+
+func _on_ProjectList_item_activated(index):
+	selected_item = index
+	_on_OpenButton_button_up()
